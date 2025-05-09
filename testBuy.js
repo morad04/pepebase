deedafcc80d759ac1b721a60d5dcc8ff8ee7f205
Darian Bailey <darian@nftychat.xyz>
2025-05-08 23:32:40 -0400

    update versions

--- testBuy.js
+++ testBuy.js
@@ -9,12 +9,12 @@ const account = privateKeyToAccount(process.env.PRIVATE_KEY);
 // Set max payment amount to 1 USDC in base units
 const fetchWithPayment = wrapFetchWithPayment(
   fetch,
-  account,
-  BigInt(0.1 * 10 ** 6)
+  account
+  //   BigInt(0.1 * 10 ** 7)
 );
 
 // Example API call
-const url = "https://402api.com/text-to-image?prompt=a%20blue%20ball";
+const url = "https://402api.com/word-count?text=hello%20world";
 
 fetchWithPayment(url, {
   method: "GET",
